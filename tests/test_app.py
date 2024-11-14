97d7179169407aa034ccfcae395b27dade4d77d2
Ricardo Tassio Dantas <ricardotassio@gmail.com>
2024-11-14 13:52:48 -0500

    chore: add schema

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from src.app import app
-
-client = TestClient(app)
-
-
-def test_read_root_should_return_hello_world():
+def test_read_root_should_return_hello_world(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Hello': 'World'}
+    assert response.json() == {'message': 'Hello World!'}
