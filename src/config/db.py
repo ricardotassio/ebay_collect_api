e943ff2bf25ac704f24c2f05d6dd88a60d959425
Ricardo Tassio Dantas <ricardotassio@gmail.com>
2024-11-14 17:43:36 -0500

    chore: add config folder

--- src/config/db.py
+++ src/config/db.py
@@ -1,19 +1,21 @@
 import os
-from pymongo import MongoClient
+
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 # Load environment variables
 load_dotenv()
 
 # Environment variables for MongoDB
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB = os.getenv("MONGO_DB")
-MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+MONGO_URI = os.getenv('MONGO_URI')
+MONGO_DB = os.getenv('MONGO_DB')
+MONGO_COLLECTION = os.getenv('MONGO_COLLECTION')
 
 # Create the MongoDB client and connect to the collection
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
 collection = db[MONGO_COLLECTION]
+
 
 # Optionally, add a function to access the collection if needed
 def get_collection():
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv('API_KEY')
